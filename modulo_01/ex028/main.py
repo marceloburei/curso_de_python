d9929eb703792f4916b4fe58c15a5e4add317256
marceloburei <bureimarcelo@gmail.com>
2023-12-20 16:27:19 -0300

    aula 10

---
+++ modulo_01/ex028/main.py
@@ -0,0 +1,14 @@
+from random import choice
+n=float(input('tente sua sorte na blaze: '))
+
+n1=[1,2,3,4,5]
+
+
+n2=choice(n1)
+
+if n >= n2:
+    print(f'parabens vc acertou   ')
+
+else:
+    print('foi por pouco, mias sorte na proxima')
+print('--fim--')
---
+++ modulo_01/ex029/main.py
@@ -0,0 +1,6 @@
+velocidade=float(input('digite a velocidade: '))
+
+if velocidade>= 80:
+    n=velocidade-80
+    print(f'seu carro sera multado em {n*7}')
+print('parabens continue assim')
