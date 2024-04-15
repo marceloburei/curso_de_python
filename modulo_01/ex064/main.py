0869b77ae517d619ea2dea4e72c6e927afa769a7
marceloburei <bureimarcelo@gmail.com>
2024-04-15 10:37:48 -0300

    curso de programaçao aula 13

---
+++ modulo_01/ex064/main.py
@@ -0,0 +1,11 @@
+total = cont = 0
+
+n2 = int(input("digite um numero: "))
+while n2 != 999:
+    cont += 1
+    total += n2
+    n2 = int(input("digite um numero: "))
+
+print(f"vc digitou {cont} numeros e a soma deles È: {total}")
+
+print("fim")
